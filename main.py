dc8afa8f8a5e5ce2e46cb967dcbdc52c02028fc3
Adason <ad.cristyan@gmail.com>
2023-10-11 01:08:39 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtratc(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtratc,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  
+  num1 = float(input("Enter first number: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+  
+  while should_continue:
+    operation_symbol = input("pick an operation: ")
+    num2 = float(input("whats the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+
+calculator()
